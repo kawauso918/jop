b6830fe3ea49801f9eaadf5c18eef0c1d6aad33c
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-02-12 10:06:00 +0000

    Rspec

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,8 +2,22 @@
 
 require 'rails_helper'
 
-describe 'モデルのテスト' do
-  it "有効な投稿内容の場合は保存されるか" do
-    expect(FactoryBot.build(:list)).to be_valid
-  end
-end
+RSpec.describe PhotoImage, "モデルに関するテスト", type: :model do
+  describe '実際に保存してみる' do
+	    it "有効な投稿内容の場合は保存されるか" do
+	      expect(FactoryBot.build(:photo_image)).to be_valid
+	    end
+	  end
+	  context "空白のバリデーションチェック" do
+	    it "titleが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+	      photo_image = PhotoImage.new(name: '', caption:'hoge')
+	      expect(photo_image).to be_invalid
+	      expect(photo_image.errors[:name]).to include("can't be blank")
+	    end
+	    it "bodyが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+	      photo_image = PhotoImage.new(name: 'hoge', caption:'')
+	      expect(photo_image).to be_invalid
+	      expect(photo_image.errors[:caption]).to include("can't be blank")
+	    end
+	  end
+	end
