61cf3004148fe53a89244c8e724e0adb3bb9099e
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-03-03 08:41:47 +0000

    Move code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -75,5 +75,19 @@ $(document).on('turbolinks:before-cache', function(){
   displayCalendar();
 });
 
+$(function() {
+    $('.menu-trigger').on('click', function(event) {
+      $(this).toggleClass('active');
+      $('#sp-menu').fadeToggle();
+      event.preventDefault();
+    });
+});
 
-
+$(function() {
+    $('#back a').on('click',function(event){
+      $('body, html').animate({
+        scrollTop:0
+      }, 800);
+      event.preventDefault();
+    });
+});
