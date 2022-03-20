f9f9d8296b90356d208543746dc6f6bfea7dda44
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-03-20 10:19:35 +0000

    Fix notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,5 +51,13 @@ class User < ApplicationRecord
     super && (self.is_deleted == false)
   end
 
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ", current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(visited_id: id, action: 'follow')
+      notification.save if notification.valid?
+    end
+  end
+
 
 end
